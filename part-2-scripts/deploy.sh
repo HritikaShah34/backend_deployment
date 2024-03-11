4e31bc4893b6a8af6b17809394cb107e8d6e545b
HritikaShah34 <125557297+HritikaShah34@users.noreply.github.com>
2024-03-11 15:11:00 +0530

    Update deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.7.0/bin/
 
 cd week-9
  git pull origin master
  cd server
  pm2 kill
- /home/ubuntu/.nvm/versions/node/v21.7.0/bin/pm2 start index.js
+ pm2 start index.js
 
 # vi deploy.sh
 #  preess i
